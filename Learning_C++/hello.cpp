e52cdb9a811824a67a0c728b481ea23e598d8b90
piyushmukeshmehta <piyush.mehta@strath.ac.uk>
2016-10-13 11:31:38 -0500

    Panel_Method

--- Learning_C++/hello.cpp
+++
@@ -1,9 +0,0 @@
-// Comment: prints "Hello world!" and an OS-independent newline
-  #include <string>    // Defines type std::string
-  #include <iostream>  // Defines global object std::cout
-  using namespace std; // Allow std:: to be dropped
-  int main() {         // Execution starts here
-    string s="Hello world!\n"; // Declares object s of type string
-    cout << s;         // An expression as a statement, << is the output operator
-    return 0;          // Execution ends here
-  }
--- Learning_C++/helloworld.cpp
+++
@@ -1,8 +0,0 @@
-#include <math.h>
-#include <matrix.h>
-#include <mex.h>
-void mexFunction(int nlhs, mxArray *plhs[], int nrhs, const mxArray *prhs[])
-{
-  mexPrintf("Hello World!\n");
-}
-- See more at: http://www.shawnlankton.com/2008/03/getting-started-with-mex-a-short-tutorial/#sthash.XZ0RinKv.dpuf
